898f31be5f323c47ca0e0f77c08a0b94d5bf7c50
Alan Gu <alan.guj@outlook.com>
2016-07-22 20:47:53 +0800

    增加requirement

--- install_flask.sh
+++
@@ -1,8 +0,0 @@
-#Flask环境部署
-packages=(Flask flask-sqlalchemy sqlalchemy-migrate requests PyMySQL flask-login flask-wtf requests \
-    flask-oauthlib flask-session qrcode image flask-cors)
-#packages=(Flask)
-for p in ${packages[@]};do
-    echo -----------------install $p-----------------------------
-    ./flask/bin/pip install $p
-done
